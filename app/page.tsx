74b25d9a8541fc6984afb92e146350160623cb24
TahmidTheDev <tahmidwhafi@gmail.com>
2025-01-19 20:30:38 +0600

    Assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function page() {
-  return <div className="text-3xl font-black  text-light-500  ">page</div>;
+  return (
+    <>
+      <div className="text-3xl font-black text-light-500 ">page</div>
+      <div className="h1-bold font-space-grotesk ">page</div>
+    </>
+  );
 }
 
 export default page;
