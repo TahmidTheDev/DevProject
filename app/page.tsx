1d62b8d88aa6084707cc107825c773d3f425c7db
TahmidTheDev <tahmidwhafi@gmail.com>
2025-01-23 10:58:57 +0600

    light and dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-
 function page() {
   return (
     <>
-      <div className="text-3xl font-black text-light-500 ">page</div>
-      <div className="h1-bold font-space-grotesk ">page</div>
+      <h1 className="h1-bold font-space-grotesk  ">page</h1>
     </>
   );
 }
